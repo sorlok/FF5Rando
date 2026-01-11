8c8d137cdbbbe964df9c7e784caa0746413932b9
Seth N. Hetu <seth.hetu@gmail.com>
2026-01-11 11:11:48 -0500

    Basic plugin project for Visual Studio

---
+++ MyFF5Plugin/Marquee.cs
@@ -0,0 +1,169 @@
+﻿using AsmResolver.PE.Exports;
+using Il2CppSystem.Asset;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace MyFF5Plugin
+{
+    // I'm trying to make a basic HUD that says "you got an item".
+    // I borrowed most of this from Magicite, since I *really* don't know Unity.
+    public sealed class Marquee : MonoBehaviour
+    {
+        private Texture2D _blackTexture;
+        private GUIStyle _guiStyle;
+
+        // Message to show
+        private string message = null;
+
+        // State of the message:
+        //   0 = fade in
+        //   1 = show message
+        //   2 = fade out
+        //   3 = pseudo-state, "done" with fade out
+        private int state = 0;
+
+        // Current alpha value, 0.0 to 1.0
+        private float alpha = 0.0f;
+
+        // The time the current state started
+        DateTime timerStart;
+
+        // How many seconds for each phase (seconds)
+        private static float[] StateTimers = { 0.25f, 10.0f, 0.25f };
+
+
+        // I guess these are required by Unity?
+        public Marquee(IntPtr ptr) : base(ptr)
+        {
+        }
+        //
+        public static Marquee Instance { get; set; }
+
+        public void Awake()
+        {
+            try
+            {
+                // Oh, maybe other files use Marquee.Instance() instead of saving it...
+                Instance = this;
+
+                //
+                // TODO: I don't know how to make something *always* appear on top in Unity.
+                //       If I can't figure it out, we can simply delete/recreate this every time
+                //       the main "Game" scene is made active.
+                //
+
+                // Create our black texture (used for the background)
+                _blackTexture = new Texture2D(1, 1);
+                _blackTexture.SetPixel(0, 0, Color.black);
+                _blackTexture.Apply();
+                _blackTexture.hideFlags = HideFlags.HideAndDontSave;  // Otherwise it'll get discarded on Scene change.
+
+                // Create our font info (used to write a message)
+                _guiStyle = new GUIStyle();
+                _guiStyle.fontSize = 24;
+                _guiStyle.normal.textColor = Color.white;
+                _guiStyle.alignment = TextAnchor.MiddleCenter;
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log.LogError($"Marquee class unable to initialize GUI elements: {ex}");
+            }
+
+            // Don't process OnGUI() until we say to.
+            enabled = false;
+        }
+
+        // Called externally; show a message!
+        public void ShowMessage(string message)
+        {
+            this.message = message;
+            this.state = 0;
+            this.timerStart = DateTime.Now;
+            this.alpha = 0.0f;
+
+            // Begin showing the GUI
+            enabled = true;
+        }
+
+        public void Update()
+        {
+            // TODO: This is where we will (presumably) count down and fade out the message (or pull the next one
+            //       from the queue to display).
+            // Can probably just cheat and use DateTime.Now (if we can't figure out how the engine stores it)
+            //   ...I guess that means if they tab out they'll miss it?
+
+            // Are we processing a message?
+            if (message == null)
+            {
+                // TODO: Pop a new message off the queue?
+            }
+            else
+            {
+                // State change?
+                float diffTime = (float)(DateTime.Now - timerStart).TotalSeconds;
+                if (diffTime >= StateTimers[state])
+                {
+                    state += 1;
+                    timerStart = DateTime.Now;  // We'll burn the remaining ms, but it's close enough.
+                    diffTime = 0;
+                }
+
+                // React to state
+                this.alpha = 1.0f; // Represents state 1
+                if (state == 0 || state == 2)
+                {
+                    // Fading in or out
+                    alpha = diffTime / StateTimers[state];
+                    if (state == 2)
+                    {
+                        alpha = 1.0f - alpha;
+                    }
+                }
+
+                // Are we "done"?
+                if (state == 3)
+                {
+                    enabled = false;
+                }
+
+
+            }
+
+
+            
+        }
+
+        // Called to handle GUI stuff.
+        // Can be called multiple times per frame, once per GUI "event"
+        // Will be skipped if "enabled" is false
+        // I think we manually render GUI elements every frame, since it's like a DearIMGUI thing?
+        public void OnGUI()
+        {
+            // TODO: We need a reasonable way to scale the HUD, the text, and the sprites.
+            //       Maybe there's some camera auto-scaling param we can grab?
+            //       We might also just hard code it to common resolutions...
+            // ...or, does it auto-scale?
+
+            // Enforce the fade)
+            GUI.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+
+            int buffer = 10;
+
+            // Set the background texture, and stretch it over the area we want it to go over.
+            GUI.skin.box.normal.background = _blackTexture;
+            GUI.Box(new Rect(buffer, buffer, Screen.width-buffer*2, 60), GUIContent.none);
+
+            // Set our label's text.
+            GUI.Label(new Rect(buffer, buffer, Screen.width-buffer*2, 60), message, _guiStyle);
+
+            // Reset to fully opaque
+            GUI.color = Color.white;
+        }
+    }
+}
+
---
+++ MyFF5Plugin/Plugin.cs
@@ -0,0 +1,374 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Il2CppInterop.Runtime.Injection;
+using Il2CppSystem.Linq;
+using Last.Data;
+using Last.Data.User;
+using Last.Interpreter.Instructions;
+using Last.Management;
+using Last.Scene;
+using Last.Systems;
+using Last.UI;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem.Interactions;
+
+namespace MyFF5Plugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+// [BepInDependency("com.bepinex.plugin.important")] // TODO: We can depend on Magicite!
+// [BepInProcess("FF5 PR.exe")]  // TODO: We can restrict to FF5
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    // Will auto load from out own config file
+    private ConfigEntry<string> cfgCustomIntro;
+
+    // TODO: Proper state variable
+    private static UserDataManager BlahMgr;  // TODO: We can probably just use UserDataManager.Instance -- that seems to be the pattern
+    private static Il2CppSystem.Collections.Generic.List<Last.Data.User.OwnedItemData> BlahItems;
+
+
+    public override void Load()
+    {
+        Log = base.Log;
+
+        // Set up this config entry
+        cfgCustomIntro = Config.Bind("General", "CustomIntro", "Nothing to see here, folks!", "Custom message to show when starting the plugin.");
+
+        // Seems like we need to inject our class into the game so that Unity can interact with it? I guess?
+        ClassInjector.RegisterTypeInIl2Cpp<Marquee>();
+
+        // Create an instance of our Marquee, so that we can interact with it later.
+        // You can access this later via Marquee.Instance()
+        // I *think* this causes it to be added to the scene and awakened.
+        string name = typeof(Marquee).FullName;
+        GameObject singleton = new GameObject(name);
+        // Don't show this in the hierarchy, don't save it to the Scene, don't unload it via "UnloadUnusedAssets()"
+        // Our script will manage this singleton entirely.
+        singleton.hideFlags = HideFlags.HideAndDontSave;
+        // If we don't do this, Unity will remove the GameObject when we change scenes
+        GameObject.DontDestroyOnLoad(singleton);
+        // Add the Marquee as a resource (component)
+        Marquee component = singleton.AddComponent<Marquee>();
+        if (component is null)
+        {
+            GameObject.Destroy(singleton);
+            throw new Exception($"The object is missing the required component: {name}");
+        }
+
+
+        // Try patching methods with Harmony
+        PatchMethods();
+
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded; custom message: {cfgCustomIntro.Value}");
+    }
+
+    private void PatchMethods()
+    {
+        try
+        {
+            Log.LogInfo("Patching methods...");
+            Harmony harmony = new Harmony("MyFF5Plugin");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Failed to patch methods.", ex);
+        }
+    }
+
+    // Try patching something random
+    // TODO: Try using HarmonyPrefix? I.e., do we have to do things this way?
+    // ...and try patching with "typeof(Patch)" to only do limited patching (instead of the entire assembly)
+    [HarmonyPatch(typeof(ResourceManager), nameof(ResourceManager.IsLoadAssetCompleted), new Type[] { typeof(string) })]
+    public static class ResourceManager_IsLoadAssetCompleted
+    {
+        public static void Postfix(string addressName, ResourceManager __instance)
+        {
+            // Yep, this works!
+            // Log.LogInfo($"XXXXX =====> ResourceManager::IsLoadAssetCompleted() called for: {addressName}");
+
+        }
+    }
+
+
+    // Save pointer?
+    [HarmonyPatch(typeof(OwnedItemClient), nameof(OwnedItemClient.AddOwnedItem), new Type[] { typeof(int), typeof(int) })]
+    public static class OwnedItemClient_AddOwnedItem
+    {
+        public static void Prefix(int contentId, int count, OwnedItemClient __instance)
+        {
+            // Save it!
+            Log.LogInfo($"XXXXX =====> OwnedItemClient::AddOwnedItem[1]({contentId},{count}) called for: {__instance.Pointer}");
+        }
+    }
+    //
+    [HarmonyPatch(typeof(OwnedItemClient), nameof(OwnedItemClient.AddOwnedItem), new Type[] { typeof(Last.Data.Master.Content), typeof(int) })]
+    public static class OwnedItemClient_AddOwnedItem2
+    {
+        public static void Prefix(Last.Data.Master.Content targetData, int count, OwnedItemClient __instance)
+        {
+            Log.LogInfo($"XXXXX =====> OwnedItemClient::AddOwnedItem[2](({targetData.MesIdName},{targetData.TypeId},{targetData.Id}),{count}) called for: {__instance.Pointer}");
+        }
+    }
+    //
+    [HarmonyPatch(typeof(OwnedItemClient), nameof(OwnedItemClient.CreateOwnedItem), new Type[] { typeof(Last.Data.Master.Content), typeof(int) })]
+    public static class OwnedItemClient_CreateOwnedItem
+    {
+        public static void Prefix(Last.Data.Master.Content content, int count, OwnedItemClient __instance)
+        {
+            Log.LogInfo($"XXXXX =====> OwnedItemClient::CreateOwnedItem[](({content.MesIdName},{content.TypeId},{content.Id}),{count}) called for: {__instance.Pointer}");
+        }
+    }
+
+
+    // Trying to understand this
+    [HarmonyPatch(typeof(OwnedItemClient), nameof(OwnedItemClient.RemoveOwnedItem), new Type[] { typeof(int), typeof(int) })]
+    public static class OwnedItemClient_RemoveOwnedItem
+    {
+        public static void Prefix(int contentId, int count, OwnedItemClient __instance)
+        {
+            // Save it!
+            Log.LogInfo($"XXXXX =====> OwnedItemClient::RemoveOwnedItem[1]({contentId},{count}) called.");
+        }
+    }
+    [HarmonyPatch(typeof(OwnedItemClient), nameof(OwnedItemClient.RemoveOwnedItem), new Type[] { typeof(int), typeof(int), typeof(int) })]
+    public static class OwnedItemClient_RemoveOwnedItem2
+    {
+        public static void Prefix(int itemType, int itemId, int count, OwnedItemClient __instance)
+        {
+            // Save it!
+            Log.LogInfo($"XXXXX =====> OwnedItemClient::RemoveOwnedItem[2]({itemType}, {itemId},{count}) called.");
+        }
+    }
+
+    // These IDs are working perfectly, no need to add/sub 1
+    // Elixir, 1, 13
+    // Leather Armor, 3, 40
+    // Whip, 2, 104
+
+
+    // Grabbing an item
+    /*
+    [HarmonyPatch(typeof(ShopUtility), nameof(ShopUtility.BuyItem), new Type[] { typeof(int), typeof(int) })]
+    public static class ShopUtility_BuyItem
+    {
+        public static void Prefix(int productId, int count)
+        {
+            Log.LogInfo($"XXXXX =====> ShopUtility.BuyItem({productId} , {count})");
+            //return true;
+        }
+    }*/
+    //
+    [HarmonyPatch(typeof(ShopUtility), nameof(ShopUtility.BuyItem), new Type[] { typeof(ShopProductData), typeof(int) })]
+    public static class ShopUtility_BuyItem2
+    {
+        public static void Prefix(ShopProductData data, int count)
+        {
+            Log.LogInfo($"XXXXX =====> ShopUtility.BuyItem[pre]({data.ProductId} , {count})");
+            InspectItems();
+
+            // TEMP: Try giving the player an Iron Armor
+            int myId = 0;
+            foreach (var item in BlahMgr.normalOwnedItems)
+            {
+                Log.LogInfo($"######## ({item.key}) => {item.value.Name} , {item.value.Count}");
+                myId = item.key;
+                break;
+            }
+
+            // Here we GOOOOO
+            // TODO: They had something important to say about making 'new' items...
+            // TODO: Actually, we should probably just use one of the many "add item" API calls in the game...
+            //ShopUtility.BuyItem(89, 1);  // Oops, infinite loop when called within a shop (duh!)
+            //External.Item.GetItem();
+            //OwnedItemData newItem = new OwnedItemData((IntPtr)myId);
+            //BlahMgr.normalOwnedItems.Add(myId, newItem);
+
+
+            //BlahMgr.normalOwnedItems.Add(89)
+        }
+        public static void Postfix(ShopProductData data, int count)
+        {
+            Log.LogInfo($"XXXXX =====> ShopUtility.BuyItem[post]({data.ProductId} , {count})");
+            InspectItems();
+        }
+        private static void InspectItems()
+        {
+            // Trying to figure out *how* an item is bought.
+            // TODO: WIP!
+            Log.LogInfo($"UserDataManager::normalItems PTR: {BlahMgr.normalOwnedItems.Pointer}");
+            Log.LogInfo($"Saved Cloned Item List PTR: {BlahItems.Pointer}");
+            Log.LogInfo($"UserDataManager::normalItems COUNT: {BlahMgr.normalOwnedItems.Count}");
+            Log.LogInfo($"Saved Cloned Item List COUNT: {BlahItems.Count}");
+        }
+    }
+
+    // TODO: Maybe patch the get/set for normalOwnedItems in general?
+    // NOTE: We don't call 'GetAllOwnedItemsClone' or 'GetImportantItemsClone' anywhere that I can see.
+    [HarmonyPatch(typeof(UserDataManager), nameof(UserDataManager.GetOwnedItemsClone))]
+    public static class UserDataManager_GetOwnedItemsClone
+    {
+        public static void Postfix(UserDataManager __instance, Il2CppSystem.Collections.Generic.List<Last.Data.User.OwnedItemData> __result)
+        {
+            if (__result == null)
+            {
+                // Log.LogInfo($"XXXXX =====> UserDataManager::GetOwnedItemsClone() => <null>");
+            }
+            else
+            {
+                // Log.LogInfo($"XXXXX =====> UserDataManager::GetOwnedItemsClone() => {__result.Count} , {__instance.normalOwnedItems.Count}");
+                // Log.LogInfo($"XXXXX =====> UserDataManager::GetOwnedItemsClone() PTR {__result.Pointer} , {__instance.normalOwnedItems.Pointer}");
+            }
+
+            // TEMP: SAVE
+            BlahMgr = __instance;
+            BlahItems = __result;
+        }
+    }
+
+
+
+
+    // TODO: ITEMS with {__instance.NormalOwnedItemList}   --- can we just save it on "set"?
+
+
+
+    // TODO: Testing
+    [HarmonyPatch(typeof(SceneLoadTask), nameof(SceneLoadTask.LoadUnityScene), new Type[] { typeof(string), typeof(string), typeof(UnityEngine.SceneManagement.LoadSceneMode) })]
+    public static class SceneLoadTask_LoadUnityScene
+    {
+        public static void Prefix(string loadAssetGroup, string loadSceneName, UnityEngine.SceneManagement.LoadSceneMode loadSceneMode, SceneLoadTask __instance)
+        {
+            //Log.LogInfo($"XXXXX =====> SceneLoadTask::LoadUnityScene({loadSceneName})");
+        }
+    }
+
+    // Also
+    [HarmonyPatch(typeof(SceneManager), nameof(SceneManager.ChangeScene))]
+    public static class SceneManager_ChangeScene
+    {
+        public static void Prefix(SceneManager __instance)
+        {
+            //Log.LogInfo($"XXXXX =====> SceneManager::ChangeScene[pre] => {__instance.currentSceneName}");
+        }
+        public static void Postfix(SceneManager __instance)
+        {
+            //Log.LogInfo($"XXXXX =====> SceneManager::ChangeScene[post] => {__instance.currentSceneName}");
+        }
+    }
+
+
+    /*
+    // TODO: See what this does:
+    public Il2CppSystem.Collections.IEnumerator OpenTreasureBox(Last.Entity.Field.FieldTresureBox entity)
+    Member of Last.Map.EventAction
+
+    // Not sure why this is here and a string:
+public static string OpenTresureBox { get; set; }
+    Member of Last.Map.EventActionDefine
+
+    // Track this too:
+public Il2CppSystem.Collections.IEnumerator EventOpenTresureBox(Last.Entity.Field.FieldTresureBox tresureBoxEntity, [bool after = False], [bool message = True])
+    Member of Last.Map.EventActionTreasure
+
+
+    // Would be good to track scripts with this:
+public bool RunScript(string scriptName, bool startChangeState, bool endChangeState)
+    Member of Last.Map.EventActionScript
+
+    // Try seing how this relates to the other functions (presumably it dispatches?)
+public virtual void EventOpenTresureBox(Last.Entity.Field.FieldTresureBox tresureBoxEntity, [bool after = False], [bool message = True])
+    Member of Last.Map.EventProcedure
+    */
+
+
+    // TODO: Testing constructor patching
+    // Note that the default Constructor is never called.
+    [HarmonyPatch(typeof(OwnedItemClient), MethodType.Constructor, new[] { typeof(IntPtr)})]
+    public class GameFrameworkCtorPatch1
+    {
+        public static void Postfix(IntPtr pointer, OwnedItemClient __instance)
+        {
+            //Log.LogInfo($"################## OwnedItemClient({pointer}) => {__instance.Pointer}");
+        }
+    }
+
+
+    // TODO: Later: getter/setter
+    // [HarmonyPatch(typeof(TestClass), "GameInstance", MethodType.Getter)]
+
+
+    // Does this do what I think?
+    [HarmonyPatch(typeof(MainGame), nameof(MainGame.Update))]
+    public class MainGame_Update
+    {
+        public static void Prefix(MainGame __instance)
+        {
+            // Yes, this is called all the time. 
+            // Will have to be careful what we put here.
+            bool isDown = UnityEngine.Input.GetKeyDown(KeyCode.F9);
+            string isDownKey = "";
+            if (isDown)
+            {
+                isDownKey = "F9";
+                Log.LogInfo($"!!! INPUT: {isDownKey}");
+                
+                // Give us some items!
+                // Note that, in reality, we may want to avoid doing this while they're in the Item menu (or Battle Item Menu) or a Shop.
+                // The game seems robust against this, but it *does* seem confusing.
+                OwnedItemClient client = new OwnedItemClient();
+                client.AddOwnedItem(243, 1); // Thief's Gloves
+
+                // Show it!
+                Marquee.Instance.ShowMessage($"Got an item: {"Thief's Gloves"}!");
+            }
+        }
+    }
+
+
+
+
+
+
+    // How about this?
+    [HarmonyPatch(typeof(ItemWindowView), nameof(ItemWindowView.SetDescriptionText), new Type[] { typeof(string), typeof(bool), typeof(bool) })]
+    public static class ItemWindowView_SetDescriptionText
+    {
+        public static void Prefix(ref string text, bool isParameter, bool isForcedChange, ItemWindowView __instance)
+        {
+            // Yep, this works too!
+            //Log.LogInfo($"XXXXX =====> ItemWindowView::SetDescriptionText() called for: {text}");
+            if (text.Length > 0)
+            {
+                //text = "Hacked hacked hacked!";
+
+                // LOL, this works here, but it tries to spend money and refuses once you're out.
+                // Amazing!
+                //ShopUtility.BuyItem(89, 1);
+                // Ok, it works now. Just pull from "content.csv"
+
+                // Let's just make our own?
+                // Yep, this seems to work.
+                //OwnedItemClient client = new OwnedItemClient();
+                //
+                //client.AddOwnedItem(235, 1); // Iron Armor
+                //client.RemoveOwnedItem(2, 10); // Remove all Potions
+            }
+        }
+    }
+
+
+
+
+}
+
